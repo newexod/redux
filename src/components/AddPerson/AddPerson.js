6f964faad53102139c42d7175702ac984e8cab0c
newexod <dimapulenko@gmail.com>
2019-03-17 16:23:26 +0300

    Setting Up the Project

---
+++ src/components/AddPerson/AddPerson.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import './AddPerson.css';
+
+class AddPerson extends Component {
+    state = {
+        name: '',
+        age: ''
+    }
+
+    nameChangedHandler = (event) => {
+        this.setState({name: event.target.value});
+    }
+
+    ageChangedHandler = (event) => {
+        this.setState({age: event.target.value});
+    }
+
+    render () {
+        return (
+            <div className="AddPerson">
+                <button onClick={() => this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>
+            </div>
+        );
+    }
+}
+
+export default AddPerson;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import counterReducer from './store/reducers/counter';
-import resultReducer from './store/reducers/result';
+import reducer from './store/reducer';
 
-const rootReducer = combineReducers({
-  ctr: counterReducer,
-  res: resultReducer
-});
-
-const store = createStore(rootReducer);
+const store = createStore(reducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
