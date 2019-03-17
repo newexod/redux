90a2b9af3e8638501f34012834498b8a4cf689fd
newexod <dimapulenko@gmail.com>
2019-03-17 14:01:21 +0300

    Dispatching Actions

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,18 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.payload
+    };
+  }
   return state;
 };
 
@@ -15,5 +27,8 @@ const store = createStore(rootReducer);
 console.log(store.getState()); // node .\redux-basics.js
 
 // Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', payload: 10});
+console.log(store.getState());
 
 // Subscription
